eee1690569bd585b612b34eb374126207279c132
Phil Zet (Zakharchenko) <phil@philzet.com>
2020-03-25 10:49:03 -0400

    ios support
    Signed-off-by: Phil Zet (Zakharchenko) <phil@philzet.com>

---
+++ PZMarkdown/PZMarkdown.h
@@ -0,0 +1,18 @@
+//
+//  PZMarkdown.h
+//  PZMarkdown
+//
+//  Created by Phil Zet on 3/25/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PZMarkdown.
+FOUNDATION_EXPORT double PZMarkdownVersionNumber;
+
+//! Project version string for PZMarkdown.
+FOUNDATION_EXPORT const unsigned char PZMarkdownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PZMarkdown/PublicHeader.h>
+
+
